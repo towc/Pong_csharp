bd1c33ec3978bf91c184d5962b870a8cb587a7a7
Copot Matei <matei@copot.eu>
2014-12-14 11:07:01 +0100

    Added AI
    J for p1, K for p2

--- PongForm.cs
+++ PongForm.cs
@@ -48,8 +48,16 @@ namespace Pong
 
     public class Game
     {
+        private int score1 = 0,
+                    score2 = 0;
+
         private int width = 600,
-                    height = 400;
+                    height = 400,
+                    maxTop, maxBottom,
+                    speed = 3,
+                    waitUpdates = 10;
+
+        public bool needsRestart = false;
 
         private int updateFrame = 0;
 
@@ -67,20 +75,29 @@ namespace Pong
             paddle1 = new Paddle(10, height / 2, 80);
             paddle2 = new Paddle(width - 20, height / 2, 80);
 
-            ball = new Ball(width / 2, height / 2, 1);
+            ball = new Ball(width / 2, height / 2, 0.5);
+
+            maxTop = 10;
+            maxBottom = height - 10;
         }
 
         private void win1()
         {
-            paddle1 = new Paddle(10, paddle1.pos.y - 5, paddle1.size.height + 10);
+            ++score1;
 
-            ball = new Ball(width / 2, height / 2, 1);
+            if(score1 % 5 == 0) paddle1 = new Paddle(10, (int)paddle1.pos.y - 5, paddle1.size.height + 10);
+
+            needsRestart = true;
+            waitUpdates += 100;
         }
         private void win2() 
         {
-            paddle2 = new Paddle(width - 20, paddle2.pos.y - 5, paddle2.size.height + 10);
+            ++score2;
 
-            ball = new Ball(width / 2, height / 2, -1);
+            if(score2 % 5 == 0) paddle2 = new Paddle(width - 20, (int)paddle2.pos.y - 5, paddle2.size.height + 10);
+
+            needsRestart = true;
+            waitUpdates += 100;
         }
 
         public void start()
@@ -93,41 +110,80 @@ namespace Pong
         {
             ++updateFrame;
 
-            ball.update();
-
-            if (ball.vel.y > 0)
+            if (waitUpdates > 0)
             {
-                if (ball.pos.y + ball.size.height >= height) ball.vel.y *= -1;
+                --waitUpdates;
             }
             else
             {
-                if (ball.pos.y <= 0) ball.vel.y *= -1;
+                ball.update();
+            }
+
+            if (ball.vel.y > 0)
+            {
+                if (ball.pos.y + ball.size.height >= maxBottom) ball.vel.y *= -1;
+            }
+            else
+            {
+                if (ball.pos.y <= maxTop) ball.vel.y *= -1;
             }
 
             if (ball.vel.x > 0)
             {
-                if (AABB(ball, paddle2)) ball.vel.x *= -1;
+                if (AABB(ball, paddle2)) ball.hit((ball.pos.y - paddle2.pos.y) / paddle2.size.height);
                 if (ball.pos.x + ball.size.width >= width) win1();
             }
             else
             {
-                if (AABB(ball, paddle1)) ball.vel.x *= -1;
+                if (AABB(ball, paddle1)) ball.hit((ball.pos.y - paddle1.pos.y) / paddle1.size.height);
                 if (ball.pos.x + ball.size.width <= 0) win2();
             }
 
             if (updateFrame % 5 == 0)
             {
-                if (controls.down1) paddle1.pos.y += 5;
-                else if (controls.up1) paddle1.pos.y -= 5;
+                if (controls.down1 && paddle1.pos.y + paddle1.size.height < maxBottom) paddle1.pos.y += speed;
+                else if (controls.up1 && paddle1.pos.y > maxTop) paddle1.pos.y -= speed;
 
-                if (controls.down2) paddle2.pos.y += 5;
-                else if (controls.up2) paddle2.pos.y -= 5;
+                if (controls.down2 && paddle2.pos.y + paddle2.size.height < maxBottom) paddle2.pos.y += speed;
+                else if (controls.up2 && paddle2.pos.y > maxTop) paddle2.pos.y -= speed;
+
+                if (controls.auto1)
+                {
+                    if (ball.pos.y < paddle1.pos.y + paddle1.size.height / 2 &&
+                        paddle1.pos.y >= maxTop) paddle1.pos.y -= speed;
+                    if (ball.pos.y > paddle1.pos.y + paddle1.size.height / 2 &&
+                        paddle1.pos.y + paddle1.size.height <= maxBottom) paddle1.pos.y += speed;
+                }
+
+                if (controls.auto2)
+                {
+                    if (ball.pos.y < paddle2.pos.y + paddle2.size.height / 2 &&
+                        paddle2.pos.y >= maxTop) paddle2.pos.y -= speed;
+                    if (ball.pos.y > paddle2.pos.y + paddle2.size.height / 2 &&
+                        paddle2.pos.y + paddle2.size.height <= maxBottom) paddle2.pos.y += speed;
+                }
+            }
+
+            if (needsRestart)
+            {
+                needsRestart = false;
+
+                if (score2 > score1)
+                {
+                    ball = new Ball((int)(paddle1.pos.x + 11), (int)(paddle1.pos.y + paddle1.size.height / 2), 1);
+                }
+                else
+                {
+                    ball = new Ball((int)(paddle2.pos.x - 6), (int)(paddle2.pos.y + paddle2.size.height / 2), -1);
+                }
             }
         }
 
         private void render()
         {
             drawer.clearScreen();
+
+            drawer.drawScore(score1, score2);
 
             drawer.drawBall(ball);
             drawer.drawPaddle(paddle1);
@@ -142,6 +198,9 @@ namespace Pong
                 case Keys.W: controls.up1 = value; break;
                 case Keys.Down: controls.down2 = value; break;
                 case Keys.S: controls.down1 = value; break;
+
+                case Keys.J: controls.auto1 = value; break;
+                case Keys.K: controls.auto2 = value; break;
             }
         }
 
@@ -165,7 +224,7 @@ namespace Pong
 
         private DateTime lastTime;
         private TimeSpan elapsedTime = new TimeSpan(0, 0, 0);
-        private TimeSpan updateTime = new TimeSpan(0, 0, 0, 0, 10);
+        private TimeSpan updateTime = new TimeSpan(0, 0, 0, 0, 3);
 
         private void startTimer()
         {
@@ -185,6 +244,8 @@ namespace Pong
 
                 render();
 
+                Thread.Sleep(4);
+
                 lastTime = now;
             }
         }
@@ -202,13 +263,16 @@ namespace Pong
             down1 = false,
             down2 = false,
             up1 = false,
-            up2 = false;
+            up2 = false,
+            auto1 = true,
+            auto2 = false;
     }
 
     public class Drawer
     {
         private Graphics ctx;
         private SolidBrush brush;
+        private Font font = new Font("Verdana", 20);
 
         private int width, height;
 
@@ -222,23 +286,37 @@ namespace Pong
 
         public void clearScreen()
         {
-            brush = new SolidBrush(Color.FromArgb(0x20000000));
+            brush = new SolidBrush(Color.FromArgb(0x40000000));
 
             ctx.FillRectangle(brush, 0, 0, width, height);
+
+            //draw borders
+            brush = new SolidBrush(Color.LightGray);
+
+            ctx.FillRectangle(brush, 0, 5, width, 5);
+            ctx.FillRectangle(brush, 0, height - 10, width, 5);
         }
 
         public void drawPaddle(Paddle paddle)
         {
             brush = new SolidBrush(Color.White);
 
-            ctx.FillRectangle(brush, paddle.pos.x, paddle.pos.y, paddle.size.width, paddle.size.height);
+            ctx.FillRectangle(brush, (int)paddle.pos.x, (int)paddle.pos.y, paddle.size.width, paddle.size.height);
         }
 
         public void drawBall(Ball ball)
         {
             brush = new SolidBrush(Color.Red);
 
-            ctx.FillRectangle(brush, ball.pos.x, ball.pos.y, ball.size.width, ball.size.height);
+            ctx.FillRectangle(brush, (int)ball.pos.x, (int)ball.pos.y, ball.size.width, ball.size.height);
+        }
+
+        public void drawScore(int s1, int s2)
+        {
+            brush = new SolidBrush(Color.Gray);
+
+            ctx.DrawString(s1 + " ", font, brush, width / 2 - 40, 10);
+            ctx.DrawString(s2 + " ", font, brush, width / 2 + 20, 10);
         }
     }
 
@@ -269,16 +347,23 @@ namespace Pong
         public Vec pos;
         public Vec vel;
 
-        public Ball(int x, int y, int dir)
+        public Ball(int x, int y, double dir)
         {
             pos = new Vec(x, y);
-            vel = new Vec(dir, 1);
+            vel = new Vec(dir, .5);
         }
 
         public void update()
         {
             pos.x += vel.x;
             pos.y += vel.y;
+        }
+
+        public void hit(double perc)
+        {
+            vel.y += (perc - .5) / 2;
+
+            vel.x *= -1.05;
         }
 
         public Vec getPos()
@@ -293,9 +378,9 @@ namespace Pong
 
     public class Vec
     {
-        public int x, y;
+        public double x, y;
 
-        public Vec(int valueX, int valueY)
+        public Vec(double valueX, double valueY)
         {
             x = valueX;
             y = valueY;
